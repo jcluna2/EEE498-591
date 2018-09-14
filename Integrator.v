ce6401bd1d4038e3d98fc44d34f8a6d937f4d7eb
Juan Luna <jcluna@Juans-MacBook-Pro.local>
2018-09-13 22:01:14 -0700

    Added a test comment on line 21

---
+++ Integrator.v
@@ -0,0 +1,21 @@
+// integrator.vp
+//; my $bW = 8 ;
+//; my $sW = 16 ;
+//; my $eC = 8 ;
+
+module `mname` (input logic [`$bW-1`:0] d,
+                input logic             d_vaild,     // d is valid
+                output logic            d_ready,     // d is ready
+
+                output logic [`$sW-1`:0] sum,
+                output logic             s_valid,    // sum is valid
+                output logic             s_ready,    // ready for sum
+
+                input logic             clk,
+                input logic             rst );
+
+
+
+endmodule
+
+// Hello
